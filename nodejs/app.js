fedfdc7d3d0e84db06f68fe11426228e434b529f
Chanathip <bipoeman12@gmail.com>
2024-04-28 08:13:39 +0700

    proxy pass to local network 'real' device

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+const port = process.env.PORT || 3000
+require('dotenv').config()
 app.get('/', (req, res) => {
   res.send('ว่าแล้วว่าต้องมา')
 })
